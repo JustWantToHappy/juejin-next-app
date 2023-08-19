6d7b79e0d95a744005d893d346ec1884a02f69e9
啦啦啦啦啦 <1302755003@qq.com>
2023-08-19 19:55:44 +0800

    Add tailwind接入自定义样式

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,27 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      //字体
+      'juejin-font-1': '#252933',
+      'juejin-font-2': '#515767',
+      'juejin-font-3': '#8a919f',
+      'juejin-font-4': '#c2c8d1',
+      //按钮相关
+      'juejin-brand-1-normal': '#1e80ff',
+      'juejin-brand-2-hover': '#1171ee',
+      'juejin-brand-3-click': '#0060dd',
+      'juejin-brand-4-disable': '#abcdff',
+      'juejin-brand-5-light': '#eaf2ff',
+      //蒙层
+      'juejin-mask-1': 'rgba(0,0,0,0.4)',
+      'juejin-mask-2': '#fff',
+      //布局块的背景
+      'juejin-layer-1': '#fff',
+      'juejin-layer-2-1': "#f7f8fa",
+      'juejin-layer-2-2':'rgba(247,248,250,0.7)',
+      'juejin-background':'#f2f3f5'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
