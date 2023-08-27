be82f779a81d2de540f6650520069ee5aea8741f
啦啦啦啦啦 <1302755003@qq.com>
2023-08-27 21:22:14 +0800

    fix 侧边栏滚动吸附bug

--- src/components/NavTags.tsx
+++ src/components/NavTags.tsx
@@ -6,30 +6,16 @@ import type { Type } from '@/types'
 import useSwr, { Fetcher } from 'swr'
 
 const NavTags = () => {
+  const { close } = headerStore()
   const navRef = React.useRef<HTMLElement>(null)
   const [active, setActive] = React.useState('recommended')
   const fetcher: Fetcher<Type[]> = (url: string) => Get<Type[]>(url)
   const { data } = useSwr('/api/tag', fetcher)
 
-
-  React.useEffect(() => {
-    const unsubHeaderStore = headerStore.subscribe(state => {
-
-      //if (!state.close && navRef.current) {
-      //  navRef.current.style.top = '8px'
-      //} else if (navRef.current) {
-      //  navRef.current.style.top = ''
-      //}
-    })
-    return function () {
-      unsubHeaderStore()
-    }
-  }, [])
-
   return (
     <nav
       ref={navRef}
-      className='min-w-[--home-tag-width] text-lg text-juejin-font-2 p-2 layer hidden xl:block sticky aside-top '>
+      className={`min-w-[--home-tag-width] text-lg text-juejin-font-2 p-2 layer hidden xl:block sticky overflow-auto ${close ? 'aside-top' : ' top-[80px]'}`}>
       <div className='flex flex-col justify-between gap-y-[1px]'>
         {data?.map(tag => <Link
           key={tag.key}
